fa65949d524e26041a30d48e1496eb06fda2e399
aman <ak4440204@gmail.com>
2024-03-08 16:01:01 +0530

    ..

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,3 +1,5 @@
+'use client'
+
 import { NextApiRequest, NextApiResponse } from "next";
 import { StoreProduct } from "../../../type";
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
